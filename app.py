2076638ac8a20234bd3cb599eca49056b668f5b3
kun49 <89496591+kun49@users.noreply.github.com>
2021-11-17 11:32:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,9 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=msg)
+        if msg == "สวัสดี":
+        replyObj = TextSendMessage(text="เออ สวัสดี")
+    else :    
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
