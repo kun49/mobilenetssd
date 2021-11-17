c37594d9e11a6ba809c910455a168f1f0d9680af
kun49 <89496591+kun49@users.noreply.github.com>
2021-11-17 11:39:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        if msg == "สวัสดี":
+    if msg == "สวัสดี":
         replyObj = TextSendMessage(text="เออ สวัสดี")
     else :    
         line_bot_api.reply_message(rtoken, replyObj)
